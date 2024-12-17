58efff9be55e9a8256b68b12f9b84981fc9532c8
alaa <allafirst1@gmail.com>
2024-12-17 11:09:08 +0300

    sentiment analysis

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
+def load_data_finance(file_path):
+
+    """Codeium docs
+    Loads a CSV file from the given file path, parsing the Date column as a datetime.
+
+    Args:
+        file_path (str): The path to the CSV file.
+
+    Returns:
+        pd.DataFrame: A pandas DataFrame containing the loaded data.
+    """
+    
+    return pd.read_csv(file_path, parse_dates=["Date"]) 
+
 def load_data(file_path):
 
     """Codeium docs
@@ -11,5 +25,5 @@ def load_data(file_path):
     Returns:
         pd.DataFrame: A pandas DataFrame containing the loaded data.
     """
-
-    return pd.read_csv(file_path, parse_dates=["Date"]) 
+    
+    return pd.read_csv(file_path) 
